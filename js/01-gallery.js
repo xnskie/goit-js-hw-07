e9baa8cc245f7f8ce70eb92374519e3dd305a490
Vadym Surovytskyi <vsurovytskyi@gmail.com>
2022-11-11 19:14:43 +0200

    vsyo

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const refs = {
+    div: document.querySelector(".gallery"),
+};
+const imagesMarkup = createItemsMarkup(galleryItems);
+
+function createItemsMarkup(item) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<div class="gallery__item">
+        <a class="gallery__link" href="${original.value}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+        })
+        .join("");
+}
+const onContainerClick = (e) => {
+    e.preventDefault();
+
+    if (e.target.classList.contains("gallery")) {
+        return;
+    };
+    const source = e.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+        <img src="${source}"width="800" height="600">`);
+
+    instance.show();
+};
+function closeModal(event) {
+    if (event.code === 'Escape') {
+        instance.close();
+    }
+}
+
+refs.div.addEventListener("click", onContainerClick);
+refs.div.addEventListener('click', closeModal)
+refs.div.insertAdjacentHTML('beforeend', imagesMarkup)
+
+
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryOg = document.querySelector(".gallery");
+
+function createGallery(arr) {
+    const galleryItemsMarkup = arr
+    .map(({ preview, original, description }) => {
+        return `<li> <a class="gallery__item" href="${original}">
+        <img class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        title="${description}" />
+        </a>
+        </li>`;
+    })
+    .join('');
+    return galleryOg.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+}
+
+createGallery(galleryItems);
+const gallery = new SimpleLightbox('.gallery a');
 
 console.log(galleryItems);
+
